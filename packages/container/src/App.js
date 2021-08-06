80cb8d89f414c0710a983d3b4602f1786deffa6e
Benny Zanuarwan <zanuar19@gmail.com>
2021-08-07 00:46:28 +0700

    trigger invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp'
 export default () => {
   return (
     <>
-      <h1>wkwkwkwk</h1>
+      <h1>wkwkwkwk hahaha</h1>
       <hr />
       <MarketingApp />
     </>
